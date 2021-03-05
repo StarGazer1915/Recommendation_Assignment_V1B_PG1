931c90ff32446afc2ae2885f74da9473960cc834
colinvlienden <colinvlienden@users.noreply.github.com>
2021-03-05 15:02:34 +0100

    Database script

---
+++ database.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS visitors;
+
+CREATE TABLE products (
+    ID SERIAL,
+    products_id VARCHAR(255),
+    price INTEGER,
+    in_stock INTEGER,
+    active BOOLEAN,
+    recommendable BOOLEAN,
+    gender VARCHAR(255) NULL,
+    category VARCHAR(255),
+    sub_category VARCHAR(255),
+    sub_sub_category VARCHAR(255),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE visitors(
+    ID SERIAL,
+    visitor_id VARCHAR(255),
+    viewed_before VARCHAR(255),
+    PRIMARY KEY (ID)
+)
+
+
